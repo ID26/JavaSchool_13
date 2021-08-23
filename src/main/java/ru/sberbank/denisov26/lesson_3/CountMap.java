dbe14461edaaf9bfbe71d0d42508a0efd3b1650b
Ivan Denisov <workpost26@gmail.com>
2021-08-23 16:31:23 +0300

    23.08.2021-m

---
+++ src/main/java/ru/sberbank/denisov26/lesson_3/CountMap.java
@@ -0,0 +1,53 @@
+package ru.sberbank.denisov26.lesson_3;
+
+import java.util.Map;
+
+public interface CountMap<K> {
+    // добавляет элемент в этот контейнер.
+    void add(K k);
+
+    //Возвращает количество добавлений данного элемента
+    int getCount(K k);
+
+    //Удаляет элемент и контейнера и возвращает количество его добавлений(до удаления)
+    int remove(K k);
+
+    //количество разных элементов
+    int size();
+
+    //Добавить все элементы из source в текущий контейнер,
+    // при совпадении ключей,     суммировать значения
+    void addAll(CountMap<K> source);
+
+    //Вернуть java.util.Map. ключ - добавленный элемент,
+    // значение - количество его добавлений
+    Map toMap();
+
+    //Тот же самый контракт как и toMap(), только всю информацию записать в destination
+    void toMap(Map  destination);
+
+    public static void main(String[] args) {
+        CountMap map = new CountMapImpl();
+
+        map.add(10);
+        map.add(10);
+        map.add(5);
+        map.add(6);
+        map.add(5);
+        map.add(10);
+
+        map.addAll(map);
+
+        System.out.println("Map size is: " + map.size());
+        System.out.println("Remove 10 from map, quantity added same elements: " + map.remove(10));
+        System.out.println("Map size is: " + map.size());
+
+
+        int count = map.getCount(5); // 2
+        int count1 = map.getCount(6); // 1
+        int count2 = map.getCount(10); // 3
+
+        System.out.println(count + " " + count1 + " " + count2);
+
+    }
+}
---
+++ src/main/java/ru/sberbank/denisov26/lesson_3/CountMapImpl.java
@@ -0,0 +1,79 @@
+package ru.sberbank.denisov26.lesson_3;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountMapImpl<K> implements CountMap<K> {
+
+    private final Map<K, Integer> map = new HashMap<>();
+
+    // добавляет элемент в этот контейнер.
+    @Override
+    public void add(K k) {
+        if (map.containsKey(k)) {
+            int count = map.get(k);
+            map.put(k, ++count);
+        } else {
+            map.put(k, 1);
+        }
+    }
+
+    //Возвращает количество добавлений данного элемента
+    @Override
+    public int getCount(K k) {
+        if (!map.containsKey(k)) {
+            return 0;
+        }
+        return map.get(k);
+    }
+
+    //Удаляет элемент и контейнера и возвращает количество его добавлений(до удаления)
+    @Override
+    public int remove(K k) {
+        Integer count = null;
+        if (map.containsKey(k)) {
+            count = map.get(k);
+            map.remove(k);
+        }
+        return count;
+    }
+
+    //количество разных элементов
+    @Override
+    public int size() {
+        return map.size();
+    }
+
+    //Добавить все элементы из source в текущий контейнер,
+    // при совпадении ключей,     суммировать значения
+    @Override
+    public void addAll(CountMap<K> source) {
+        if (source == null) {
+            return;
+        }
+        for (Object o : source.toMap().keySet()) {
+            K key = (K) o;
+            if (map.containsKey(key)) {
+                int count = map.get(key) + (int) source.toMap().get(key);
+                map.put(key, count);
+            } else {
+                map.put(key, 1);
+            }
+        }
+
+
+    }
+
+    //Вернуть java.util.Map. ключ - добавленный элемент,
+    // значение - количество его добавлений
+    @Override
+    public Map toMap() {
+        return map;
+    }
+
+    //Тот же самый контракт как и toMap(), только всю информацию записать в destination
+    @Override
+    public void toMap(Map destination) {
+        destination = map;
+    }
+}
